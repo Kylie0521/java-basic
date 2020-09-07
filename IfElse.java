3f6dc57fea514b8c1558c6464a5354ea75c5423f
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-07 16:35:20 -0700

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -9,16 +9,15 @@ public class IfElse{
 
 		int n = scan.nextInt();
 
-
-		if(n >= 90 && n <= 100) {
+                if(n >= 90 && n <= 100) {
 			System.out.println("A");
 
 		} else if(n >= 80 && n < 90) {
-		    System.out.println("B");
-	    }else if(n >= 70 && n < 80) {
-		    System.out.println("C");
-		}else if(n >= 60 && n < 70) {
-		    System.out.println("C: Work Harder Next Time!");
-		}
-	}
+		   System.out.println("B");
+	        }else if(n >= 70 && n < 80) {
+		   System.out.println("C");
+	        }else if(n >= 60 && n < 70) {
+		   System.out.println("C: Work Harder Next Time!");
+	        }
+       }
 }
