3d8f77eabba428dbc1ef819a788b7cc1e527caa2
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-07 13:43:41 -0700

    Add files via upload

---
+++ BooleanVab.java
@@ -0,0 +1,26 @@
+public class BooleanVab{
+	public static void main(String[] args){
+		boolean passedDoor = true;
+		boolean missedDoor = false;
+		boolean passedAllDoors = false;
+		int doorCount = 0;
+	if(misssedDoor){
+	   System.out.println("We passed the first door!");
+	   doorCount = doorCount +1;
+	}
+	if(passeddDoor){
+	   System.out.println("We passed the second door!");
+	   doorCount = doorCount +1;
+	}
+        if(passedDoor){
+	   System.out.println("We passed the first door!");
+	   doorCount = doorCount +1;
+	}
+	if(DoorCount == 3){
+	   passedAllDoors = true;
+	}
+	if(passedAllDoors){
+            System.out.println("Congratualations you won the program!");
+	}
+  }
+}
