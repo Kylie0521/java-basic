278ecbeeb8be8b07cfe8a04b355ed46e575bb5b5
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-08 15:56:24 -0700

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,26 @@
+public class Demo{
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i =1; i <=100; i++){
+			if(i%7==0 | containsSeven(x) ==7| getSecondLastNumber(x) ==7|getThirdLastNumber(x)==7{
+				return true;
+			}else{
+				return false;
+			}
+		}
+		System.out.println(count);
+	}
+
+	static int getLastNumber(int x){
+		return x%10;
+	}
+
+	static int getSecondLastNumber(int x){
+		return (x/10)%10；
+	}
+
+	static int getThirdLastNumber(int x){
+		return x/100;
+	}
+}
+
