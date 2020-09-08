f9c64f0b2f5759d15958b05d02e2efd11695f71f
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-07 17:12:07 -0700

    Add files via upload
    while(true)

---
+++ Loop basic/DoWhileLoops.java
@@ -0,0 +1,11 @@
+public class DoWhileLoops{
+	public static void main(String[] args) {
+		int a = 0;
+		do{
+			System.out.println("a");
+		}While(a == 1);
+	}
+}
+
+//prints 0 : Because when While(False) Loop stoped, however, when a == 0
+//then prints uncountable a 
