073fd75900a64e7915514d059f0a499fc7088a07
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-08 14:24:24 -0700

    Add files via upload
    How many integers form 1 to 100 can be divided by 3

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo{
+	public static void main(String[] args){
+		int count = 0;
+		for (int i=1, i<= 100, i++){
+			if (i%3==0){
+				count++;
+			}
+		}
+
+	    System.out.println(count);
+		
+	}
+
+}
