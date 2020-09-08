7092211a7859c755e22e080191b3cf95276e3d1a
Kylie0521 <65053134+Kylie0521@users.noreply.github.com>
2020-09-08 15:36:32 -0700

    Update CountInteger.java

--- CountInteger.java
+++ CountInteger.java
@@ -3,7 +3,7 @@
 public class CountInteger{
 	public static void main(String[] args){
 		int count = 0;
-		for (int i=1, i<= 100, i++){
+		for (int i=1; i<= 100; i++){
 			if (i%3==0){
 				count++;
 			}
